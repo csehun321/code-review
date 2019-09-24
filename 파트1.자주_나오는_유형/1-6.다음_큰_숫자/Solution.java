cf0df05abd8fd69ec2226f306eceb59860e86e4e
ag502 <ag502@naver.com>
2019-09-25 00:56:00 +0900

    1-6 문제풀이

--- 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
+++ 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
@@ -1,5 +1,27 @@
 public class Solution {
+    private int numOfOne(final int n) {
+        int num = n;
+        int numOfOne = 0;
+        while(num > 0) {
+            if (num % 2 == 1) {
+                numOfOne++;
+            }
+            num = num / 2;
+        }
+        return numOfOne;
+    }
+
     public int solution(int n) {
-        return 0;
+        int answer = 0;
+        int startNum = n + 1;
+
+        while(true) {
+            if (numOfOne(n) == numOfOne(startNum)) {
+                answer = startNum;
+                break;
+            }
+            startNum++;
+        }
+        return answer;
     }
 }
