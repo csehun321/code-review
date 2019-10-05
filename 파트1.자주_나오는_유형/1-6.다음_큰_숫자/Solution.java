1e88d3e71cdc7dfd41ca3718c1a7298a20b94cc9
Song Chiwon <iamchiwon@gmail.com>
2019-10-06 03:25:52 +0900

    restore merges source

--- 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
+++ 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
@@ -1,28 +1,6 @@
 public class Solution {
-    private int numOfOne(final int n) {
-        int num = n;
-        int numOfOne = 0;
-        while (num > 0) {
-            if (num % 2 == 1) {
-                numOfOne++;
-            }
-            num = num / 2;
-        }
-        return numOfOne;
-    }
-
     public int solution(int n) {
         int answer = 0;
-        int startNum = n + 1;
-
-        int currentNum = numOfOne(n);
-        while (true) {
-            if (currentNum == numOfOne(startNum)) {
-                answer = startNum;
-                break;
-            }
-            startNum++;
-        }
         return answer;
     }
 }
