60e67b6c54e930d75ba5a43962e673745bcd720e
DericLee9306 <37926928+DericLee9306@users.noreply.github.com>
2020-02-05 23:54:56 +0900

    [4기 이경민][1-3]방문 길이
    방문 길이 문제 풀이

--- 파트1.자주_나오는_유형/1-3.방문길이/Solution.java
+++ 파트1.자주_나오는_유형/1-3.방문길이/Solution.java
@@ -1,5 +1,50 @@
-public class Solution {
+class Solution {
     public int solution(String dirs) {
-        return 0;
+        int answer = 0;
+
+        int[][][][] map = new int[11][11][11][11];
+        char[] cmd = dirs.toCharArray();
+
+        int x = 5;
+        int y = 5;
+
+        for(int i = 0; i < cmd.length; i++){
+            int afterX = x;
+            int afterY = y;
+
+            switch (cmd[i]){
+                case 'U':
+                    afterY += 1;
+                    break;
+                case 'D':
+                    afterY -= 1;
+                    break;
+                case 'R':
+                    afterX += 1;
+                    break;
+                case 'L':
+                    afterX -= 1;
+                    break;
+                default:
+            }
+
+            if(rangeCheck(afterX, afterY)) continue;
+
+            if(map[x][y][afterX][afterY] == 0){
+                map[x][y][afterX][afterY] = 1;
+                map[afterX][afterY][x][y] = 1;
+
+                answer += 1;
+            }
+
+            x = afterX;
+            y = afterY;
+        }
+
+        return answer;
+    }
+
+    private Boolean rangeCheck(int x, int y){
+        return (x < 0 || x > 10 || y < 0 || y > 10);
     }
 }
