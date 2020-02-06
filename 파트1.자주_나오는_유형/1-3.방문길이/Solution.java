b3b7db8472bab9e8276e7865562da60957aa4278
DericLee9306 <37926928+DericLee9306@users.noreply.github.com>
2020-02-06 22:57:52 +0900

    [4기 이경민][1-3]방문 길이 수정

--- 파트1.자주_나오는_유형/1-3.방문길이/Solution.java
+++ 파트1.자주_나오는_유형/1-3.방문길이/Solution.java
@@ -5,46 +5,72 @@ class Solution {
         int[][][][] map = new int[11][11][11][11];
         char[] cmd = dirs.toCharArray();
 
-        int x = 5;
-        int y = 5;
+        Point point = new Point(5, 5);
+    		Path path = new Path();
 
         for(int i = 0; i < cmd.length; i++){
-            int afterX = x;
-            int afterY = y;
+            int afterX = point.x;
+            int afterY = point.y;
 
             switch (cmd[i]){
                 case 'U':
-                    afterY += 1;
+                    afterY += path.up;
                     break;
                 case 'D':
-                    afterY -= 1;
+                    afterY += path.down;
                     break;
                 case 'R':
-                    afterX += 1;
+                    afterX += path.right;
                     break;
                 case 'L':
-                    afterX -= 1;
+                    afterX += path.left;
                     break;
                 default:
             }
 
             if(rangeCheck(afterX, afterY)) continue;
 
-            if(map[x][y][afterX][afterY] == 0){
-                map[x][y][afterX][afterY] = 1;
-                map[afterX][afterY][x][y] = 1;
+            if(map[point.x][point.y][afterX][afterY] == 0){
+                map[point.x][point.y][afterX][afterY] = 1;
+                map[afterX][afterY][point.x][point.y] = 1;
 
                 answer += 1;
             }
 
-            x = afterX;
-            y = afterY;
+            point.move(afterX, afterY);
         }
 
-        return answer;
-    }
+            return answer;
+        }
 
-    private Boolean rangeCheck(int x, int y){
-        return (x < 0 || x > 10 || y < 0 || y > 10);
-    }
+        private Boolean rangeCheck(int x, int y){
+            return (x < 0 || x > 10 || y < 0 || y > 10);
+        }
+
+        class Point {
+            int x = 0;
+            int y = 0;
+
+            public Point(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void move(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+
+        }
+
+      	class Path {
+      		int up = 1;
+              int down = -1;
+      		int right = 1;
+              int left = -1;
+
+              public Path(){
+
+              }
+      	}
 }
