c0538dae12f85497a7b286d8cc263fed2c59963f
김봉근 <kimbong-geun@gimbong-geun-ui-MacBookPro.local>
2020-05-11 22:50:22 +0900

    1-1

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -1,6 +1,28 @@
 public class Solution {
     public bool solution(int x) {
-        System.out.println("테스트");
-        return false
+        boolean answer = true;
+
+        int checkTempNum1 = 0;
+        int checkTempNum2 = 0;
+        int checkTempNum3 = x;
+
+        while (true) {
+            checkTempNum1 += checkTempNum3 % 10;
+            checkTempNum2 = checkTempNum3 / 10;
+
+            if (checkTempNum2 < 10) {
+                break;
+            }
+
+            checkTempNum3 = checkTempNum2;
+        }
+
+        int checkTempSum = checkTempNum1 + checkTempNum2;
+
+        if ( x % checkTempSum != 0 ){
+            answer = false;
+        }
+
+        return answer;
     }
 }
