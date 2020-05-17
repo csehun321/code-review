48a5a3c24cda566a0d4de37a1298f43d2064e8f2
dabitk <dabitk@gmail.com>
2020-05-18 05:22:12 +0900

    solve 2-1

--- 파트2.자료구조/2-1.올바른_괄호/Solution.java
+++ 파트2.자료구조/2-1.올바른_괄호/Solution.java
@@ -1,5 +1,27 @@
-public class Solution {
-    public boolean solution(String s) {
-        return false;
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                stack.push(s.charAt(i));
+            }else{
+                if(!stack.isEmpty() && stack.peek()=='('){
+                    stack.pop();
+                }else{
+                    answer = false;
+                }
+            }
+        }
+        
+        if(!stack.isEmpty()){
+            answer = false;
+        }
+        
+
+        return answer;
     }
 }
