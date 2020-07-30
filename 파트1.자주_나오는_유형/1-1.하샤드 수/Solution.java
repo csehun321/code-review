af30a36ba433e31af015e12746ccc32a9d510897
박무권 <moo7200@naver.com>
2020-07-30 23:46:39 +0900

    commit on '1-1'

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -1,6 +1,17 @@
 public class Solution {
-    public bool solution(int x) {
-        boolean answer = true;
-        return answer;
+    public boolean solution(int x) {
+
+        int x_copy=x;
+        int num=0;
+
+        while(x_copy>0){
+            num+=x_copy/10;
+            x_copy/=10;
+        }
+        
+        //System.out.println("x"+x);
+        //System.out.println("num"+num);
+
+        return x/num==0?true:false;
     }
 }
