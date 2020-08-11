58ea1f0a6a8b23cd4e03ec2aab37480e55d4f4be
Song Chiwon <iamchiwon@gmail.com>
2020-08-11 19:37:05 +0900

    이 레파지토리에 머지하시면 안됩니다.

--- 파트2.자료구조/2-1.올바른_괄호/Solution.java
+++ 파트2.자료구조/2-1.올바른_괄호/Solution.java
@@ -3,26 +3,6 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Stack<Character> stack = new Stack<Character>();
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
-                stack.push(s.charAt(i));
-            }else{
-                if(!stack.isEmpty()){
-                    stack.pop();
-                }else{
-                    answer = false;
-                    break;
-                }
-            }
-        }
-        
-        if(!stack.isEmpty()){
-            answer = false;
-        }
-        
-
         return answer;
     }
 }
