145da3a203e4a721bd576eb74371892a25affeeb
Song Chiwon <iamchiwon@gmail.com>
2020-10-12 12:05:05 +0900

    Update Solution.java

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -1,17 +1,6 @@
-public class Solution {
+class Solution {
     public boolean solution(int x) {
-
-        int x_copy=x;
-        int num=0;
-
-        while(x_copy>0){
-            num+=x_copy/10;
-            x_copy/=10;
-        }
-        
-        //System.out.println("x"+x);
-        //System.out.println("num"+num);
-
-        return x/num==0?true:false;
+        boolean answer = true;
+        return answer;
     }
 }
