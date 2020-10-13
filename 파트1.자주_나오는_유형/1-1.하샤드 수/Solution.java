1cca2d793416950498bc1653ba90ad600d31d8e6
csehun321 <71028490+csehun321@users.noreply.github.com>
2020-10-13 20:29:03 +0900

    Update Solution.java

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -1,6 +1,15 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        return answer;
+        String temp = Integer.toString(x);
+        int[] digits = new int[temp.length()];
+        
+        int sum = 0;
+        
+        for (int i = 0; i < temp.length(); i++){
+            digits[i] = temp.charAt(i) - '0';
+            sum += digits[i];
+        }
+        
+        return x%sum==0;
     }
 }
