a9516f540b62532b822db5730e04315e71ffd19c
csehun321 <71028490+csehun321@users.noreply.github.com>
2020-10-14 19:09:21 +0900

    하샤드수 솔루션코드

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean solution(int x) {
         int num = 0;
         if(x<10){
-            return true;
+            return true; //한자리수는 하샤드 수임
         }
         else if(x>=10 && x<100){
-            num = x/10 + x%10;
+            num = x/10 + x%10; //두자리수는 10으로 나눈 몫과 나머지를 합침
             if (x%num==0)
-                return true;
+                return true; 
         }
         else if(x>=100 && x<1000){
-            num = x/100 + (x%100)/10 + (x%100)%10;
+            num = x/100 + (x%100)/10 + (x%100)%10; //세자리수는 100으로 나눈 몫과 100으로 나눈 나머지를 10으로 나눈 몫과 나머지를 모두 더한다
             if (x%num==0)
-                return true;
+                return true; 
         }
         else if(x>=1000 && x<10000){
-            num = x/1000 + (x%1000)/100 + ((x%1000)%100)/10 + ((x%1000)%100)%10;
+            num = x/1000 + (x%1000)/100 + ((x%1000)%100)/10 + ((x%1000)%100)%10; //네자리수도 마찬가지 방법으로 진행
             if (x%num==0)
                 return true;
         }
