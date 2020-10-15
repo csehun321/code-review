53cf061ac16ea277df14370212bf54b6fabc1d4b
csehun321 <csehun321@gmail.com>
2020-10-15 22:49:45 +0900

    다음 큰 수 솔루션코드 2차수정

--- 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
+++ 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
@@ -2,22 +2,22 @@ class Solution {
     public int solution(int n) {
         int one = 0;
 
-        one = count_one(n);
+        one = CountOne(n);
         while(true){
             ++n;
-            if(one==count_one(n)){
+            if(one==CountOne(n)){
                 return n;
             }
         }
     }
 
-    public int count_one(int num) {
+    private int CountOne(int num) {
         int one = 0;
         int divided = 0;
 
         while(num != 0){
             divided = num % 2;
-            num = num / 2;
+            num = num >> 1;
             if(divided == 1) {
                 one++;
             }
