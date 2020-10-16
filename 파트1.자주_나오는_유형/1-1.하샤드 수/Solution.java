9261347b8f43aca634a437392f47d2713eeddeb8
csehun321 <csehun321@gmail.com>
2020-10-16 20:08:57 +0900

    하샤드수 솔루션코드 3차수정

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -2,15 +2,12 @@ class Solution {
     public boolean solution(int x) {
         int num = 0;
         int divided = x;
-        int left = 0;
         while(divided != 0){
+            int left;
             left = divided % 10;
             divided = divided/10;
             num = num + left;
         }
-        if (x%num==0)
-            return true;
-        else
-            return false;
+        return x%num==0;
     }
 }
