15a9cf40853fa733db6301b8bd0d1bbd6bd04d4a
csehun321 <csehun321@gmail.com>
2020-10-16 23:26:23 +0900

    영어 끝말잇기 솔루션코드

--- 파트1.자주_나오는_유형/1-4.영어_끝말잇기/Solution.java
+++ 파트1.자주_나오는_유형/1-4.영어_끝말잇기/Solution.java
@@ -1,5 +1,37 @@
-public class Solution {
+import java.util.ArrayList;
+
+class Solution {
     public int[] solution(int n, String[] words) {
-        return new int[] {};
+
+        int[] answer = new int[2];
+
+        ArrayList<String> player[] = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            player[i] = new ArrayList<String>();
+        }
+
+        int NumberOfWord = words.length;
+        int CurrentWord = 0;
+
+        while(CurrentWord != NumberOfWord) {
+            for(int i = 0; i < n; i++){
+                if(CurrentWord > 0 && words[CurrentWord].charAt(0) != words[CurrentWord-1].charAt(words[CurrentWord-1].length()-1)){
+                    answer[0] = i + 1;
+                    answer[1] = (CurrentWord / n) + 1;
+                    return answer;
+                }
+                for(int j = 0; j < n; j++) {
+                    if (player[j].contains(words[CurrentWord])) {
+                        answer[0] = i + 1;
+                        answer[1] = (CurrentWord / n) + 1;
+                        return answer;
+                    }
+                }
+                player[i].add(words[CurrentWord]);
+                CurrentWord++;
+            }
+
+        }
+        return answer;
     }
 }
