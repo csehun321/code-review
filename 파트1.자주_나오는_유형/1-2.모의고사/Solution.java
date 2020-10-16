ed0890d80c3a7d46ffc67075525d50c066b5a49e
csehun321 <csehun321@gmail.com>
2020-10-16 20:46:31 +0900

    모의고사 솔루션코드 수정

--- 파트1.자주_나오는_유형/1-2.모의고사/Solution.java
+++ 파트1.자주_나오는_유형/1-2.모의고사/Solution.java
@@ -3,30 +3,24 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] answers) {
-
-        int[] correct = new int[3];
-        int[] sort = new int[3];
         int max = 0;
 
         ArrayList<Integer> ans = new ArrayList();
 
-        int[] po1 = {1, 2, 3, 4, 5};
-        int[] po2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] po3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[][] po = {{1, 2, 3, 4, 5},{2, 1, 2, 3, 2, 4, 2, 5},{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
 
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == po1[i%5]) {
-                correct[0]++;
-                sort[0]++;
-            }
-            if (answers[i] == po2[i%8]) {
-                correct[1]++;
-                sort[1]++;
-            }
-            if (answers[i] == po3[i%10]) {
-                correct[2]++;
-                sort[2]++;
-            }
+        int student = po.length;
+        int[] correct = new int[student];
+        int[] sort = new int[student];
+
+        int LengthOfAnswer = answers.length;
+
+        for (int i = 0; i < student; i++) {
+            for(int j = 0; j < LengthOfAnswer; j++)
+                if (answers[j] == po[i][j%po[i].length]) {
+                    correct[i]++;
+                    sort[i]++;
+                }
         }
         Arrays.sort(sort);
         max = sort[sort.length-1];
