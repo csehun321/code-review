cc47d23bf23a0d9dbfc38a32395d4885c3f94f7d
csehun321 <csehun321@gmail.com>
2020-10-17 21:33:03 +0900

    K번째수 솔루션코드

--- 파트1.자주_나오는_유형/1-7.K번째수/Solution.java
+++ 파트1.자주_나오는_유형/1-7.K번째수/Solution.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
 public class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        return new int[] {};
+        ArrayList<Integer> sub = new ArrayList<>();
+        ArrayList<Integer> number = new ArrayList<>();
+
+        int NumberOfCommands = commands.length;
+
+        for(int i = 0; i < NumberOfCommands; i++){
+            int start = commands[i][0]-1;
+            int end = commands[i][1];
+            for(int j = start; j < end; j++){
+                sub.add(array[j]);
+            }
+            sub.sort(null);
+            number.add(sub.get(commands[i][2]-1));
+            sub.clear();
+        }
+
+        int[] ans = new int[number.size()];
+        int size = 0;
+        for(int temp : number){
+            ans[size++]=temp;
+        }
+        return ans;
     }
 }
