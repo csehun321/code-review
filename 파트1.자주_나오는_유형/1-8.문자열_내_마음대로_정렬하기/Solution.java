2fc518d25a7ff187fa79440e6ec1ee73c8b5a55c
csehun321 <csehun321@gmail.com>
2020-10-17 21:56:17 +0900

    문자열 내 마음대로 정렬하기 솔루션코드

--- 파트1.자주_나오는_유형/1-8.문자열_내_마음대로_정렬하기/Solution.java
+++ 파트1.자주_나오는_유형/1-8.문자열_내_마음대로_정렬하기/Solution.java
@@ -1,5 +1,18 @@
-public class Solution {
+import java.util.Arrays;
+class Solution {
     public String[] solution(String[] strings, int n) {
-        return new String[] {};
+        int NumberOfStrings = strings.length;
+
+        for(int i = 0; i < NumberOfStrings; i++){
+            strings[i] = strings[i].substring(n,n+1).concat(strings[i]);
+        }
+
+        Arrays.sort(strings);
+
+        for(int i = 0; i < NumberOfStrings; i++){
+            strings[i] = strings[i].substring(1,strings[i].length());
+        }
+
+        return strings;
     }
 }
