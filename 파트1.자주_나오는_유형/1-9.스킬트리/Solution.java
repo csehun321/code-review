01410344531efbc7d23ad7fe55fb2fc086971e4d
csehun321 <csehun321@gmail.com>
2020-10-17 23:03:22 +0900

    스킬트리 솔루션코드

--- 파트1.자주_나오는_유형/1-9.스킬트리/Solution.java
+++ 파트1.자주_나오는_유형/1-9.스킬트리/Solution.java
@@ -1,5 +1,30 @@
-public class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        return 0;
+import java.util.ArrayList;
+class Solution {
+    public int solution(String skill, String[] SkillTree) {
+        int answer = 0;
+        int NumberOfTree = SkillTree.length;
+
+        ArrayList<Integer> index = new ArrayList<>();
+
+        for(int i = 0; i < NumberOfTree; i++){
+            int lengthOfSkill = skill.length();
+            for(int j = 0; j < lengthOfSkill; j++){
+                if(!SkillTree[i].contains(skill.substring(j,j+1)))
+                    index.add(SkillTree[i].length());
+                else
+                    index.add(SkillTree[i].indexOf(skill.charAt(j)));
+            }
+            answer++;
+
+            for(int k = 1; k < lengthOfSkill; k++){
+                if(index.get(k-1)>index.get(k)){
+                    answer--;
+                    break;
+                }
+            }
+            index.clear();
+        }
+
+        return answer;
     }
 }
