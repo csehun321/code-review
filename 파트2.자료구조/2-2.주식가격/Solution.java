c5c91b94f55310d5d809410edfb5e7361f8ea93c
csehun321 <csehun321@gmail.com>
2020-10-18 21:53:43 +0900

    주식가격 솔루션코드

--- 파트2.자료구조/2-2.주식가격/Solution.java
+++ 파트2.자료구조/2-2.주식가격/Solution.java
@@ -1,5 +1,17 @@
-public class Solution {
+class Solution {
     public int[] solution(int[] prices) {
-        return new int[] {};
+        int length = prices.length;
+        int[] time = new int[length];
+        for (int i = 0; i < length-1; i++) {
+            for (int j = i+1; j < length; j++) {
+                if (prices[i] > prices[j]) {
+                    time[i] = j-i;
+                    break;
+                }
+                if (j==time.length-1)
+                    time[i] = j-i;
+            }
+        }
+        return time;
     }
 }
