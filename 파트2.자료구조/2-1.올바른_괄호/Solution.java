1662f4da3b962bb00ec2d4d92861d1e88ef8bcdb
csehun321 <csehun321@gmail.com>
2020-10-18 18:13:24 +0900

    올바른 괄호 솔루션코드

--- 파트2.자료구조/2-1.올바른_괄호/Solution.java
+++ 파트2.자료구조/2-1.올바른_괄호/Solution.java
@@ -1,8 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        return answer;
+        ArrayList<Integer> stack = new ArrayList<>();
+        int length = s.length();
+        for(int i = 0; i < length; i++){
+            if(s.charAt(i)=='('){
+                stack.add(0);
+            }
+            else if(s.charAt(i)==')'){
+                if(stack.size()==0){
+                    return false;
+                }
+                stack.remove(stack.size()-1);
+            }
+        }
+        return stack.size()==0;
     }
 }
