e665cc5481909641361fcd2a02bda109d832bd98
csehun321 <csehun321@gmail.com>
2020-10-18 18:45:23 +0900

    완주하지 못 한 선수 솔루션코드

--- 파트2.자료구조/2-5.완주하지 못한 선수/Solution.java
+++ 파트2.자료구조/2-5.완주하지 못한 선수/Solution.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution {
     public String solution(String[] participant, String[] completion) {
-        return "";
+        ArrayList<String> player = new ArrayList<>();
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int NumberOfPlayer = participant.length;
+        for(int i = 0; i < NumberOfPlayer-1; i ++){
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[NumberOfPlayer-1];
     }
 }
