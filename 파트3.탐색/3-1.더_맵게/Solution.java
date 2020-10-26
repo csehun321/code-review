2438dfd84462241ddc60338fb764cfe3ca3705d7
csehun321 <csehun321@gmail.com>
2020-10-26 18:56:25 +0900

    더 맵게 솔루션 코드

--- 파트3.탐색/3-1.더_맵게/Solution.java
+++ 파트3.탐색/3-1.더_맵게/Solution.java
@@ -1,5 +1,22 @@
-public class Solution {
+import java.util.PriorityQueue;
+
+class Solution {
     public int solution(int[] scoville, int K) {
-        return 0;
+        int answer = 0;
+        PriorityQueue<Integer> scov = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            scov.add(scoville[i]);
+        }
+        while(scov.peek() < K) {
+            if(scov.size() == 1)
+                return -1;
+            int MostMin = scov.poll();
+            int seconMin= scov.poll();
+            int mix = MostMin + (seconMin*2);
+            scov.add(mix);
+            answer ++;
+        }
+        return answer;
     }
 }
