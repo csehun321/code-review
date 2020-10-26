a24ce08dc429f8ec580ecea9ff3d73e18376af90
csehun321 <csehun321@gmail.com>
2020-10-26 16:12:59 +0900

    기능개발 솔루션코드

--- 파트2.자료구조/2-3.기능개발/Solution.java
+++ 파트2.자료구조/2-3.기능개발/Solution.java
@@ -1,5 +1,40 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
 public class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        return new int[] {};
+        int NumOfProgress = progresses.length;
+        Queue<Integer> time = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        int cnt = 0;
+        for(int i = 0; i < NumOfProgress; i++){
+            while(progresses[i]<100) {
+                progresses[i] += speeds[i];
+                cnt++;
+            }
+            time.offer(cnt);
+            cnt = 0;
+        }
+        int prevQueue = time.poll();
+        int cnt2 = 1;
+        while(!time.isEmpty()){
+            int nextQueue = time.poll();
+            if(prevQueue >= nextQueue){
+                cnt2++;
+            }
+            if(prevQueue < nextQueue){
+                prevQueue = nextQueue;
+                ans.add(cnt2);
+                cnt2 = 1;
+            }
+        }
+        ans.add(cnt2);
+        int SizeOfAnswer = ans.size();
+        int[] answer = new int[SizeOfAnswer];
+        for(int i = 0; i < SizeOfAnswer; i++){
+            answer[i] = ans.get(i);
+        }
+
+        return answer;
     }
 }
